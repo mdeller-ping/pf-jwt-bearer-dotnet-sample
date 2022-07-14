b5468d6dfa233fadad2a13d8e08373d6b9735153
Michael Deller <mdeller@pingidentity.com>
2022-07-14 10:34:46 -0400

    Initial

---
+++ pf-jwt-bearer-dotnet-sample/Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Begin JSON Web Token Stuff
+
+builder.Services.AddAuthentication(opt => {
+    opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.Authority = "https://mdeller-pingfederate-engine.ping-devops.com";
+    options.Audience = "https://mdeller-pingfederate-engine.ping-devops.com";
+});
+
+// End JSON Web Token Stuff
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
